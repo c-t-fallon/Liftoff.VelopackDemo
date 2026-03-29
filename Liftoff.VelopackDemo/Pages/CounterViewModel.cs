c2311c5ddcd1fffbc346cc6949e10cf548f9ce62
c-t-fallon <corey.t.fallon@gmail.com>
2026-03-29 08:15:56 -0400

    Restructure solution: add Abstractions & Components projects
    Refactored solution to introduce Liftoff.VelopackDemo.Abstractions (for shared interfaces like ICounterViewModel) and Liftoff.VelopackDemo.Components (for reusable Blazor components). Updated DI to use ICounterViewModel, moved drawing models and viewer to Components, and split Counter page logic for better separation. Adjusted project references, namespaces, and imports accordingly. No functional changes; improves modularity and testability.

--- Liftoff.VelopackDemo/Pages/CounterViewModel.cs
+++ Liftoff.VelopackDemo/Pages/CounterViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Liftoff.VelopackDemo.Abstractions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Liftoff.VelopackDemo.Pages;
 
-public partial class CounterViewModel : ObservableObject
+public partial class CounterViewModel : ObservableObject, ICounterViewModel
 {
     [ObservableProperty]
     private int currentCount = 0;
